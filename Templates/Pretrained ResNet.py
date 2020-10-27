ed6bc7eaae4833ae11f38f8da6f621708621d654
ElijahCoding <cuiyeqing960904@gmail.com>
2020-10-27 22:31:32 +0300

    using resnet model

---
+++ Templates/Pretrained ResNet.py
@@ -0,0 +1,24 @@
+from __future__ import print_function, division
+from builtins import range, input
+# Note: you may need to update your version of future
+# sudo pip install -U future
+
+from keras.layers import Input, Lambda, Dense, Flatten
+from keras.models import Model
+from keras.applications.resnet50 import ResNet50, preprocess_input
+# from keras.applications.inception_v3 import InceptionV3, preprocess_input
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import matplotlib.pyplot as plt
+
+from glob import glob
+
+# resize all the images to this
+IMAGE_SIZE = [224, 224]
+
+epochs = 16
+batch_size = 32
+
