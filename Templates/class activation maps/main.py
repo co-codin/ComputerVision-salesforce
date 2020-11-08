b829c5b37451c4d68851f152ef85661d7b641f36
ElijahCoding <cuiyeqing960904@gmail.com>
2020-11-08 16:03:14 +0300

    built

--- Templates/class activation maps/main.py
+++ Templates/class activation maps/main.py
@@ -1,5 +1,5 @@
 from keras.models import Model
-from keras.applications.resnet50 import ResNet50, preprocess_input
+from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
 from keras.preprocessing import image
 
 import numpy as np
@@ -27,3 +27,35 @@ model = Model(inputs=resnet.inputs, outputs=activation_layer.output)
 final_dense = resnet.get_layer('fc1000')
 W = final_dense.get_weights()[0]
 
+while True:
+    img = image.load_img(np.random.choice(image_files), target_size=(224, 224))
+    x = preprocess_input(np.expand_dims(img, 0))
+    fmaps = model.predict(x)[0] # 7 x 7 x 2048
+
+    # get predicted class
+    probs = resnet.predict(x)
+    classnames = decode_predictions(probs)[0]
+    classname = classnames[0][1]
+    pred = np.argmax(probs[0])
+
+    # get the 2048 weights for the relevant class
+    w = W[:, pred]
+
+    # 'dot' w with fmaps
+    cam = fmaps.dot(w)
+
+    # upsample to 224 x 224
+    # 7 x 32 = 224
+    cam = sp.ndimage.zoom(cam, (32, 32), order=1)
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(img, alpha=0.8)
+    plt.imshow(cam, cmap='jet', alpha=0.5)
+    plt.subplot(1, 2, 2)
+    plt.imshow(img)
+    plt.title(classname)
+    plt.show()
+
+    ans = input("Continue? (Y/n)")
+    if ans and ans[0].lower() == 'n':
+        break
