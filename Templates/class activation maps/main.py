aabac399224a01d75474afb6bcd6aedde9fd9b69
ElijahCoding <cuiyeqing960904@gmail.com>
2020-11-08 15:13:19 +0300

    making class activation layer

--- Templates/class activation maps/main.py
+++ Templates/class activation maps/main.py
@@ -10,3 +10,17 @@ from glob import glob
 
 image_files = glob('../datasets/101_ObjectCategories/*/*.jp*g')
 image_files += glob('../datasets/256_ObjectCategories/*/*.jp*g')
+
+plt.imshow(image.load_img(np.random.choice(image_files)))
+plt.show()
+
+# add preprocessing layer to the front of VGG
+resnet = ResNet50(input_shape=(224, 224, 3), weights='imagenet', include_top=True)
+
+resnet.summary()
+
+# make a model to get output before flatten
+activation_layer = resnet.get_layer('activation_49')
+
+model = Model(inputs=resnet.inputs, outputs=activation_layer.output)
+
