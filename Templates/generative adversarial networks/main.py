e3982bb6cc2cb442c3d73d403199032fad502993
ElijahCoding <cuiyeqing960904@gmail.com>
2020-11-11 18:34:59 +0300

    data preprocessing

---
+++ Templates/generative adversarial networks/main.py
@@ -0,0 +1,24 @@
+from tensorflow.keras.layers import Input, Dense, LeakyReLU, Dropout, BatchNormalization
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import SGD, Adam
+
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys, os
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# map inputs to (-1, +1) for better training
+x_train, x_test = x_train / 255.0 * 2 - 1, x_test / 255.0 * 2 - 1
+print("x_train.shape: ", x_train.shape)
+
+N, H, W = x_train.shape
+D = H * W
+
+x_train, x_test = x_train.reshape(-1, D), x_test.reshape(-1, D)
+
+print("x_train.shape: ", x_train.shape)
